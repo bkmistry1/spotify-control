8b4fe791f43ce7b5e88ca445323c1dda47978581
bkmistry1 <bmistry1@stevens.edu>
2024-02-28 15:35:33 -0500

    -changed songselect to dict

--- spotify_views.py
+++ spotify_views.py
@@ -16,16 +16,18 @@ class songSelectList(Select):
     def __init__(self, options, trackInfo, spotifyUser) -> None:
         super().__init__(placeholder="Select at least 1", options=options, min_values=1, max_values=len(options))
         self.trackInfo = trackInfo
-        self.selectedUri = []
+        self.selectedSongs = {}
         self.spotifyUser = spotifyUser
 
     async def callback(self, interaction: discord.Interaction):
+        await interaction.response.defer(ephemeral=True)
+        msg = await interaction.original_response()
         songSelectionString = "\n"
-        self.selectedUri = []
+        self.selectedSongs = {}
         for index, songSelected in enumerate(self.values):
-            self.selectedUri.append(self.trackInfo[songSelected])
+            self.selectedSongs[songSelected] = self.trackInfo[songSelected]
             songSelectionString += str(index) + ". " + songSelected + "\n"
-        await interaction.response.edit_message(content="Selected: " + songSelectionString)
+        await msg.edit(content="Selected: " + songSelectionString)
         return
     
 class songSelectButton(Button):
@@ -38,8 +40,8 @@ class songSelectButton(Button):
         msg = await interaction.original_response()
         songsAdded = 0
         await msg.edit(content="Added: " + str(songsAdded), view=None)
-        for songUri in self.selectMenu.selectedUri:
-            await addSongToQueue(spotifyUser=self.selectMenu.spotifyUser, songUri=songUri)            
+        for song in self.selectMenu.selectedSongs.keys():
+            await addSongToQueue(spotifyUser=self.selectMenu.spotifyUser, songUri=self.selectMenu.selectedSongs[song])
             songsAdded += 1
             await msg.edit(content="Added: " + str(songsAdded), view=None)
             await asyncio.sleep(1)
