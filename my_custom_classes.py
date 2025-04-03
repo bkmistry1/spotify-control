8abaed04b02ad1e0a39474e69a7f0263abeed37d
bkmistry1 <bmistry1@stevens.edu>
2025-04-02 21:48:02 -0400

    -added getLength method to SongNode class

--- my_custom_classes.py
+++ my_custom_classes.py
@@ -18,3 +18,13 @@ class SongNode():
     
     async def getSongName(self):
         return self.name
+    
+    async def getLength(self):
+        length = 1
+        currentNode = self
+        while(currentNode.next is not None):
+            currentNode = currentNode.next
+            length += 1
+
+        return length
+            
